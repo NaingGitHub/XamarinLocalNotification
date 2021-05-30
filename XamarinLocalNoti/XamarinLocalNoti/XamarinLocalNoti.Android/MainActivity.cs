658ad033e653ade083fc5873c34746b618ca95ca
NaingThura Aung <naingthuraaung2018@gmail.com>
2021-05-30 22:46:29 +0630

    Local Notification

---
+++ XamarinLocalNoti/XamarinLocalNoti/XamarinLocalNoti.Android/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Plugin.LocalNotification;
+
+namespace XamarinLocalNoti.Droid
+{
+    [Activity(Label = "XamarinLocalNoti", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            // Must create a Notification Channel when API >= 26
+            // you can created multiple Notification Channels with different names.
+            NotificationCenter.CreateNotificationChannel(new Plugin.LocalNotification.Platform.Droid.NotificationChannelRequest
+            {
+                Importance = NotificationImportance.High
+            });
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ XamarinLocalNoti/XamarinLocalNoti/XamarinLocalNoti/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Plugin.LocalNotification;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinLocalNoti
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void NotiTimePicker_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            NotificationRequest notiObj = new NotificationRequest
+            {
+                Title = "Testing Local Noti",
+                Description = "Hello there! this is sample noti.",
+                NotificationId = 100,
+                Repeats = isRepeat.IsChecked ? NotificationRepeat.TimeInterval : NotificationRepeat.No,
+                NotifyRepeatInterval = new TimeSpan(0, 0, 0, 10),
+                NotifyTime = DateTime.Now.AddSeconds(10)
+            };
+            NotificationCenter.Current.Show(notiObj);
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            NotificationCenter.Current.CancelAll();
+        }
+    }
+}
